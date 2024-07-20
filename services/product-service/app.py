cea16085c15603ef914da747bb623bcc3568e298
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-07-20 14:05:40 -0400

    aitdevops

---
+++ services/product-service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/products')
+def products():
+    return {
+        "products": [
+            "Rolls-royce",
+            "Bently",
+            "Maybach"
+        ]
+    }
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
